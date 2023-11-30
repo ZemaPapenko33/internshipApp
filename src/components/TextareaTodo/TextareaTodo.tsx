05a228f87600737ce22e609fd5d7dc44b6f1f3a6
ZemaPapenko33 <zakharpapenko@gmail.com>
2023-11-30 10:12:42 +0300

    fix: add overflow-wrap for textarea

--- src/components/TextareaTodo/TextareaTodo.tsx
+++ src/components/TextareaTodo/TextareaTodo.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TextareaTodoWrapper } from './TextareaTodoStyled';
 
 interface ITodoPayload {
--- src/components/TextareaTodo/TextareaTodoStyled.ts
+++ src/components/TextareaTodo/TextareaTodoStyled.ts
@@ -4,6 +4,7 @@ export const TextareaTodoWrapper = styled.textarea`
   border-width: 2px;
   border-radius: 0.25rem;
   margin-bottom: 0.5rem;
+  overflow-wrap: break-word;
   box-shadow:
     0 10px 15px -3px rgb(0 0 0 / 0.1),
     0 4px 6px -4px rgb(0 0 0 / 0.1);
